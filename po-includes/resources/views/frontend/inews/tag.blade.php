8ec8c9b8fa36870ea34b65480b415af3a6abd22c
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-11 00:00:20 +0700

    import tag view from semesta-front

--- po-includes/resources/views/frontend/inews/tag.blade.php
+++ po-includes/resources/views/frontend/inews/tag.blade.php
@@ -1,49 +1,60 @@
-@extends(getTheme('layouts.app'))
+@extends(getTheme('layouts.page'))
 
 @section('content')
-	<div class="page-title">&nbsp;</div>
-	
-	<div class="container">
-		<div class="row row-m">
-			<div class="col-sm-8 col-p main-content">
-				<div class="theiaStickySidebar">
-					<div class="post-inner">
-						<div class="post-head">
-							<h2 class="title">{{ $tag->title }} ({{ $posts->total() }})</h2>
-						</div>
-						
-						<div class="post-body">
-							@foreach($posts as $post)
-								<div class="news-list-item articles-list">
-									<div class="img-wrapper">
-										<a href="{{ prettyUrl($post) }}" class="thumb"><img src="{{ getPicture($post->picture, 'medium', $post->updated_by) }}" alt="" class="img-responsive"></a>
-									</div>
-									<div class="post-info-2">
-										<h4><a href="{{ prettyUrl($post) }}" class="title">{{ $post->title }}</a></h4>
-										<ul class="authar-info">
-											<li><i class="ti-timer"></i> {{ date('d F Y' , strtotime($post->created_at)) }}</li>
-											<li><a href="{{ prettyUrl($post) }}" class="link"><i class="ti-eye"></i>{{ $post->hits }} Views</a></li>
-										</ul>
-										<p class="hidden-sm">{{ \Str::limit(strip_tags($post->content), 150) }}</p>
-									</div>
-								</div>
-							@endforeach
-						</div>
-						
-						<div class="post-footer"> 
-							<div class="row thm-margin">
-								<div class="col-xs-12 col-sm-12 col-md-12 thm-padding">
-									{{ $posts->links() }}
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			
-			<div class="col-sm-4 col-p rightSidebar">
-				@include(getTheme('partials.sidebar'))
-			</div>
-		</div>
-	</div>
+    <!-- Page Image Hero -->
+    <div class="big-hero page-hero-section">
+        <img src="{{ isset($category)? getPicture($category->picture, 'medium', $category->updated_by): asset('po-content/uploads/bg-sekolah.jpg') }}" alt="Image-1" class="page-img w-full h-inherit object-center object-cover">
+        <div class="page-hero flex justify-center items-center h-inherit absolute top-0 w-full">
+            <div class="hero-section">
+                <h1 class="page-hero-title">Tags : {{ $tag->title }} ({{ $posts->total() }})</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <!-- Main -->
+        <div class="wrapper mt-7">
+            <div class="wrapper-content">
+                <!-- Post Category -->
+                <div class="berita-container">
+                    <div class="berita-terbaru-container">
+                        <div class="berita-terbaru-header">
+                            <h1 class="berita-terbaru-title">Tags : {{ $tag->title }} ({{ $posts->total() }})</h1>
+                        </div>
+
+                        <div class="berita-terbaru-content">
+                            <div class="berita-wrapper">
+                                @foreach ($posts as $post)
+                                    <a href="{{ prettyUrl($post) }}" class="berita-terbaru-1 berita-terbaru">
+                                        <div class="card">
+                                            <div class="card-title">
+                                                <img src="{{ getPicture($post->picture, 'medium', $post->updated_by) }}" alt="{{ $post->title }}" class="berita-img">
+                                            </div>
+                                            <div class="card-content">
+                                                <h1 class="berita-title">
+                                                    {{ $post->title }}
+                                                </h1>
+                                                <span class="berita-sub">
+                                                    <ion-icon name="person" class="relative author-icon"></ion-icon> <span class="author">{{ $post->name }}</span>
+                                                    <ion-icon name="calendar" class="relative date-post-icon"></ion-icon> <span class="date-posted">{{ date('d F Y', strtotime($post->created_at)) }}</span>
+                                                    <i class="fa fa-eye" aria-hidden="true"></i> <span class="date-posted">{{ $post->hits }} views</span>
+                                                </span>
+                                                <p class="berita-desc">
+                                                    {{ \Str::limit(strip_tags($post->content), 150) }}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                @endforeach
+                            </div>
+                            <div class="pagination-wrapper">
+                                {{ $posts->links() }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @include(getTheme('partials.sidebar'))
+        </div>
+    </div>
 @endsection
