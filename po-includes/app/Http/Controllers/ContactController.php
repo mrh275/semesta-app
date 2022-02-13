57a3eaa8ac330db36c9e575f53796346822cc12a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-14 00:45:25 +0700

    added custom validation messages

--- po-includes/app/Http/Controllers/ContactController.php
+++ po-includes/app/Http/Controllers/ContactController.php
@@ -9,60 +9,68 @@ use App\Contact;
 
 class ContactController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
 		config([
 			'captcha.secret' => getSetting('recaptcha_secret'),
 			'captcha.sitekey' => getSetting('recaptcha_key'),
 		]);
-		
-        // $this->middleware('auth');
-    }
 
-    /**
-     * Show the application contact.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
+		// $this->middleware('auth');
+	}
+
+	/**
+	 * Show the application contact.
+	 *
+	 * @return \Illuminate\Contracts\Support\Renderable
+	 */
+	public function index()
+	{
 		$twitterid = explode('/', getSetting('twitter'));
-		SEOTools::setTitle('Contact - '.getSetting('web_name'));
-		SEOTools::setDescription('Contact - '.getSetting('web_description'));
+		SEOTools::setTitle('Contact - ' . getSetting('web_name'));
+		SEOTools::setDescription('Contact - ' . getSetting('web_description'));
 		SEOTools::metatags()->setKeywords(explode(',', getSetting('web_keyword')));
 		SEOTools::setCanonical(getSetting('web_url') . '/contact');
-		SEOTools::opengraph()->setTitle('Contact - '.getSetting('web_name'));
-		SEOTools::opengraph()->setDescription('Contact - '.getSetting('web_description'));
+		SEOTools::opengraph()->setTitle('Contact - ' . getSetting('web_name'));
+		SEOTools::opengraph()->setDescription('Contact - ' . getSetting('web_description'));
 		SEOTools::opengraph()->setUrl(getSetting('web_url') . '/contact');
 		SEOTools::opengraph()->setSiteName(getSetting('web_author'));
-		SEOTools::opengraph()->addImage(asset('po-content/uploads/'.getSetting('logo')));
-		SEOTools::twitter()->setSite('@'.$twitterid[count($twitterid)-1]);
-		SEOTools::twitter()->setTitle('Contact - '.getSetting('web_name'));
-		SEOTools::twitter()->setDescription('Contact - '.getSetting('web_description'));
+		SEOTools::opengraph()->addImage(asset('po-content/uploads/' . getSetting('logo')));
+		SEOTools::twitter()->setSite('@' . $twitterid[count($twitterid) - 1]);
+		SEOTools::twitter()->setTitle('Contact - ' . getSetting('web_name'));
+		SEOTools::twitter()->setDescription('Contact - ' . getSetting('web_description'));
 		SEOTools::twitter()->setUrl(getSetting('web_url') . '/contact');
-		SEOTools::twitter()->setImage(asset('po-content/uploads/'.getSetting('logo')));
-		SEOTools::jsonLd()->setTitle('Contact - '.getSetting('web_name'));
-		SEOTools::jsonLd()->setDescription('Contact - '.getSetting('web_description'));
+		SEOTools::twitter()->setImage(asset('po-content/uploads/' . getSetting('logo')));
+		SEOTools::jsonLd()->setTitle('Contact - ' . getSetting('web_name'));
+		SEOTools::jsonLd()->setDescription('Contact - ' . getSetting('web_description'));
 		SEOTools::jsonLd()->setType('WebPage');
 		SEOTools::jsonLd()->setUrl(getSetting('web_url') . '/contact');
-		SEOTools::jsonLd()->setImage(asset('po-content/uploads/'.getSetting('logo')));
-		
-        return view(getTheme('contact'));
-    }
-	
+		SEOTools::jsonLd()->setImage(asset('po-content/uploads/' . getSetting('logo')));
+
+		return view(getTheme('contact'));
+	}
+
 	public function send(Request $request)
-    {
-		$this->validate($request,[
+	{
+		$this->validate($request, [
 			'name' => 'required',
 			'email' => 'required|string|max:255|email',
 			'subject' => 'required',
 			'message' => 'required',
 			'g-recaptcha-response' => 'required|captcha'
+		], [
+			'name.required' => 'Nama tidak boleh kosong',
+			'email.required' => 'Email tidak boleh kosong',
+			'email.max' => 'Email maksimal 255 karakter',
+			'email.email' => 'Email tidak valid',
+			'subject.required' => 'Subjek tidak boleh kosong',
+			'message.required' => 'Pesan tidak boleh kosong',
+			'g-recaptcha-response.required' => 'Captcha tidak boleh kosong',
 		]);
 
 		$request->request->add([
@@ -72,7 +80,7 @@ class ContactController extends Controller
 		$requestData = $request->all();
 
 		Contact::create($requestData);
-		
-		return redirect('contact')->with('flash_message', __('contact.store_notif'));
-    }
+
+		return redirect('contact')->with('flash_message', "<strong>Berhasil!</strong> Pesan anda telah terkirim. Terima kasih telah menghubungi kami.");
+	}
 }
